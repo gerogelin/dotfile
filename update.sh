ef03ce734397b1cd1ac01cd9a1b762e40b6afbbc
George Lin <lyssan10@gmail.com>
2020-10-14 11:22:50 +0800

    update the update script

--- update.sh
+++ update.sh
@@ -1,2 +1,12 @@
-cp -f ~/.vimrc .vimrc
-cp -f ~/.gitconfig .gitconfig
+if [[ $1 == 'tohome' ]]; then
+    echo "copy file to home directory"
+    cp -f ~/.vimrc .vimrc
+    cp -f ~/.gitconfig .gitconfig
+elif [[ $1 == 'togit' ]]; then
+    echo "copy files from home directory"
+    cp -f ~/.vimrc .vimrc
+    cp -f ~/.gitconfig .gitconfig
+else
+    echo "Command not support, input following command"
+    echo "./update.sh [tohome|togit]"
+fi
