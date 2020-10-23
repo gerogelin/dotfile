24bc34242cd3dadfff506461b50a107a12397eaf
George Lin <lyssan10@gmail.com>
2020-10-23 14:19:58 +0800

    ctags: add personal config to dotfile
    ctags config include the verilog and scala setting.

--- update.sh
+++ update.sh
@@ -2,10 +2,12 @@ if [[ $1 == 'tohome' ]]; then
     echo "copy file to home directory"
     cp -f ~/.vimrc .vimrc
     cp -f ~/.gitconfig .gitconfig
+    cp -f ~/.ctags .ctags
 elif [[ $1 == 'togit' ]]; then
     echo "copy files from home directory"
     cp -f ~/.vimrc .vimrc
     cp -f ~/.gitconfig .gitconfig
+    cp -f ~/.ctags .ctags
 else
     echo "Command not support, input following command"
     echo "./update.sh [tohome|togit]"
