d1fdb6836af378165ecda530e171948c8c55fc2e
George Lin <lyssan10@gmail.com>
2020-11-12 23:00:27 +0800

    ag: update the update script

--- update.sh
+++ update.sh
@@ -4,12 +4,14 @@ if [[ $1 == 'tohome' ]]; then
     cp -f ~/.gitconfig .gitconfig
     cp -f ~/.ctags .ctags
     cp -f ~/.tmux.conf .tmux.conf
+    cp -f ~/.agignore .agignore
 elif [[ $1 == 'togit' ]]; then
     echo "copy files from home directory"
     cp -f ~/.vimrc .vimrc
     cp -f ~/.gitconfig .gitconfig
     cp -f ~/.ctags .ctags
     cp -f ~/.tmux.conf .tmux.conf
+    cp -f ~/.agignore .agignore
 else
     echo "Command not support, input following command"
     echo "./update.sh [tohome|togit]"
